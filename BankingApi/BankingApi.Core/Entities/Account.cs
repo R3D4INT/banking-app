b13057b27da5d2998a56a7c42100db80745b795a
Danyil Khokhulia <khokhulya2006@gmail.com>
2025-10-30 01:04:40 +0100

    Added account entity. Added db context configuration

---
+++ BankingApi/BankingApi.Core/Entities/Account.cs
@@ -0,0 +1,18 @@
+﻿namespace BankingApi.Core.Entities;
+
+public class Account
+{
+    public Guid Id { get; set; }
+    public string OwnerName { get; set; }
+    public decimal Balance { get; set; }
+    public DateTime CreatedAt { get; set; }
+    private Account() { }
+
+    public Account(string ownerName, decimal initialBalance)
+    {
+        Id = Guid.NewGuid();
+        OwnerName = ownerName;
+        Balance = initialBalance;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
