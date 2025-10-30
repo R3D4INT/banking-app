ccb6238aa630f681f910abf7d8fbb5ab3ae2af74
Danyil Khokhulia <khokhulya2006@gmail.com>
2025-10-30 02:04:26 +0100

    Added Dtos

---
+++ BankingApi/BankingApi.Application/DTOs/AccountResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace BankingApi.Application.DTOs;
+
+public class AccountResponse
+{
+    public Guid Id { get; set; }
+    public string OwnerName { get; set; }
+    public decimal Balance { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
